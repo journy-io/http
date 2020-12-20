9f8906e1b06d2806f43ad006b2bc069192eb72c0
Hans Ott <hansott@hotmail.be>
2020-12-20 14:25:21 +0100

    🐛 Expose HttpClientNode

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unused-modules */
 export { HttpClient } from "./HttpClient";
+export { HttpClientNode } from "./HttpClientNode";
 export { HttpClientFixed } from "./HttpClientFixed";
 export { HttpClientConsoleLogging } from "./HttpClientConsoleLogging";
 export { HttpRequest } from "./HttpRequest";
